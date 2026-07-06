e3494873622cacee2d876a24617dd32a37c0292d
Dwayne Crooks <dwayne@tinyinterpreters.dev>
2026-07-06 07:11:19 -0400

    Extract testValue

--- tests/Test/CONST/Lexer.elm
+++ tests/Test/CONST/Lexer.elm
@@ -1,16 +1,16 @@
 module Test.CONST.Lexer exposing (suite)
 
 import CONST.Lexer as L
-import Expect
 import Parser as P
-import Test exposing (Test, describe, test)
+import Test exposing (Test, describe)
+import Test.Lib exposing (testValue)
 
 
 suite : Test
 suite =
     describe "CONST.Lexer"
         [ describe "digits" <|
-            List.map testDigits
+            List.map (testValue <| P.run L.digits)
                 [ ( "123", Just 123 )
                 , ( "123 ", Just 123 )
                 , ( "123  ", Just 123 )
@@ -18,27 +18,3 @@ suite =
                 , ( "onetwothree", Nothing )
                 ]
         ]
-
-
-testDigits : ( String, Maybe Int ) -> Test
-testDigits ( input, expectedOutput ) =
-    test ("\"" ++ input ++ "\"") <|
-        \_ ->
-            case P.run L.digits input of
-                Ok n ->
-                    if expectedOutput == Just n then
-                        Expect.pass
-
-                    else
-                        Expect.fail <|
-                            Debug.toString
-                                { expected = Debug.toString expectedOutput
-                                , actual = Debug.toString n
-                                }
-
-                Err e ->
-                    if expectedOutput == Nothing then
-                        Expect.pass
-
-                    else
-                        Expect.fail (Debug.toString e)
--- tests/Test/CONST/Parser.elm
+++ tests/Test/CONST/Parser.elm
@@ -2,15 +2,15 @@ module Test.CONST.Parser exposing (suite)
 
 import CONST.AST as AST exposing (..)
 import CONST.Parser as P
-import Expect
-import Test exposing (Test, describe, test)
+import Test exposing (Test, describe)
+import Test.Lib exposing (testValue)
 
 
 suite : Test
 suite =
     describe "CONST.Parser"
         [ describe "parse" <|
-            List.map testParse
+            List.map (testValue P.parse)
                 [ ( "123", Just (Program (Const 123)) )
                 , ( "123 ", Just (Program (Const 123)) )
                 , ( "123  ", Just (Program (Const 123)) )
@@ -19,27 +19,3 @@ suite =
                 , ( "onetwothree", Nothing )
                 ]
         ]
-
-
-testParse : ( String, Maybe AST.Program ) -> Test
-testParse ( input, expectedOutput ) =
-    test ("\"" ++ input ++ "\"") <|
-        \_ ->
-            case P.parse input of
-                Ok program ->
-                    if expectedOutput == Just program then
-                        Expect.pass
-
-                    else
-                        Expect.fail <|
-                            Debug.toString
-                                { expected = Debug.toString expectedOutput
-                                , actual = Debug.toString program
-                                }
-
-                Err e ->
-                    if expectedOutput == Nothing then
-                        Expect.pass
-
-                    else
-                        Expect.fail (Debug.toString e)
