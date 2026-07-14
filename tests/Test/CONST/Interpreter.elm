4c6584862ad296356489f67d7d5f4429189faae6
Dwayne Crooks <dwayne@tinyinterpreters.dev>
2026-07-14 14:02:36 -0400

    Add the "123abc" test case

--- tests/Test/CONST/Interpreter.elm
+++ tests/Test/CONST/Interpreter.elm
@@ -15,6 +15,7 @@ suite =
                 , ( "123  ", Just (VNumber 123) )
                 , ( " 123", Just (VNumber 123) )
                 , ( "  123", Just (VNumber 123) )
+                , ( "123abc", Nothing )
                 , ( "onetwothree", Nothing )
                 ]
         ]
--- tests/Test/CONST/Parser.elm
+++ tests/Test/CONST/Parser.elm
@@ -16,6 +16,7 @@ suite =
                 , ( "123  ", Just (Program (Const 123)) )
                 , ( " 123", Just (Program (Const 123)) )
                 , ( "  123", Just (Program (Const 123)) )
+                , ( "123abc", Nothing )
                 , ( "onetwothree", Nothing )
                 ]
         ]
