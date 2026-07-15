3cd7d4a7b3c40e0c993db2d2386a56fda630cc12
Dwayne Crooks <dwayne@tinyinterpreters.dev>
2026-07-15 05:30:10 -0400

    Use a more reliable method to set PROJECT_ROOT
    It is set to the absolute path to the root of the Git working tree,
    no matter which project subdirectory you run `nix develop` from.

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
           ];
 
           shellHook = ''
-            export PROJECT_ROOT="$PWD"
+            export PROJECT_ROOT="$(git rev-parse --show-toplevel)"
             export PS1="($name)\n$PS1"
 
             f () {
